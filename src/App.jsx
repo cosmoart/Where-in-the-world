416cce5cfc0a232021418acf5cabd177eadea8e4
CosmoArt <cosmohydra17@gmail.com>
2022-08-19 15:33:03 -0500

    scroll bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Cards from './components/Cards';
 import useCountries from './hooks/useCountries';
@@ -6,6 +6,19 @@ import useCountries from './hooks/useCountries';
 function App() {
 	const MAX_PAGES = 24;
 	const [loading, countries] = useCountries("https://restcountries.com/v3.1/all");
+
+	useEffect(() => {
+		let busy = false;
+		window.onscroll = function (e) {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !busy) {
+				console.log("bottom");
+				busy = true;
+				setTimeout(() => {
+					busy = false;
+				}, 600);
+			}
+		}
+	}, []);
 
 	return (
 		<>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,17 @@
 export default function Cards({ countries, MAX_PAGES }) {
-	return (
-		<>
-			{
-				countries.map((item, index) => {
-					if (index < MAX_PAGES) {
-						return <article key={index + 105}>
-							<h3>{item.name.official}</h3>
-							<ul>
-								<li>Population: {item.population}</li>
-								<li>Region: {item.region}</li>
-								<li>Capital: {item.capital[0]}</li>
-							</ul>
-						</article>
-					}
-				})
-			}
-		</>
-	)
+	return countries.map((item, index) => {
+		if (index < MAX_PAGES) {
+			return (
+				<article key={index + 105}>
+					<img src={item.flags.svg} alt={item.name.official + " flag"} />
+					<h3>{item.name.official}</h3>
+					<ul>
+						<li>Population: {item.population}</li>
+						<li>Region: {item.region}</li>
+						<li>Capital: {item.capital[0]}</li>
+					</ul>
+				</article>
+			)
+		}
+	})
 }
