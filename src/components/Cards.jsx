83618f637b2ecf2defe5a8614216ce8045d7e117
CosmoArt <cosmohydra17@gmail.com>
2022-08-19 15:04:08 -0500

    render flags info

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,20 @@
 export default function Cards({ countries, MAX_PAGES }) {
-	let page = <></>
-	console.log(countries);
-	countries.map((item, index) => {
-		if (index < MAX_PAGES) {
-			return <div>
-				<h3>{item.name.official}</h3>
-				<ul>
-					<li>Population: {item.population}</li>
-					<li>Region: {item.region}</li>
-					<li>Capital: {item.capital[0]}</li>
-				</ul>
-			</div>
-		}
-	})
+	return (
+		<>
+			{
+				countries.map((item, index) => {
+					if (index < MAX_PAGES) {
+						return <article key={index + 105}>
+							<h3>{item.name.official}</h3>
+							<ul>
+								<li>Population: {item.population}</li>
+								<li>Region: {item.region}</li>
+								<li>Capital: {item.capital[0]}</li>
+							</ul>
+						</article>
+					}
+				})
+			}
+		</>
+	)
 }
