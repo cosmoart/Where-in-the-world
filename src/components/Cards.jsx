94bd8a569473b9452387ae5298fa128b4394dfc3
CosmoArt <cosmohydra17@gmail.com>
2022-08-22 12:22:17 -0500

    icons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,7 @@ export default function Cards({ countries, MAX_PAGES }) {
 		return countries.map((item, index) => {
 			if (index < MAX_PAGES) {
 				return (
-					<article key={index + 105} tabIndex="1" className="bg-white dark:bg-darkblue hover:scale-105 transition-all rounded-md overflow-hidden cursor-pointer shadow-3xl" onClick={() => handleCard(item.name.common)}>
+					<article key={index + 105} tabIndex="0" className="countryCard bg-white dark:bg-darkblue hover:scale-105 transition-all rounded-md overflow-hidden cursor-pointer shadow-3xl" onClick={() => handleCard(item.name.common)}>
 						<img src={item.flags.svg} alt={item.name.common + " flag"} className="h-2/5 object-cover w-full" loading="lazy" />
 						<section className="p-6 h-3/5">
 							<h3 className="font-extrabold text-2xl my-3">{item.name.common}</h3>
--- src/components/CountriesForm.jsx
+++ src/components/CountriesForm.jsx
@@ -26,7 +26,7 @@ export default function CountriesForm() {
 			<div className="relative sm:w-[45%]">
 				<input ref={SearchInput} type="search" name="country" placeholder='Search for a country...' onChange={handleSearch} className="p-3 shadow-3xl pl-10 dark:bg-darkblue rounded-[4px] w-full" />
 				<button type="button" aria-label='Search' onClick={handleBtnSearch} className="absolute left-3 top-2/4 -translate-y-1/2 hover:opacity-80 active:scale-95">
-					<img src="src/assets/icons/search.svg" alt="Search" />
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0z" /></svg>
 				</button>
 			</div>
 			<select name="region" onChange={handleRegion} className="dark:bg-darkblue p-2 rounded-[4px]">
