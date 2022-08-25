58214dabeb33ec385327869abe57a81c11e26d9c
CosmoArt <cosmohydra17@gmail.com>
2022-08-24 20:42:47 -0500

    favicon dynamic

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -4,13 +4,21 @@ export default function CountryDetails({ country }) {
 	const [show, setShow] = useState(false);
 
 	useEffect(() => {
-		setShow(true)
+		setShow(true);
+		document.title = `${country.name.common} - Where in the World?`
+		document.querySelector("link[rel~='icon']").href = country.flags.svg || "src/assets/favicon/favicon.svg"
 	}, [country])
+
+	function handleBack() {
+		setShow(false);
+		document.title = `Where in the World?`;
+		document.querySelector("link[rel~='icon']").href = "src/assets/favicon/favicon.svg"
+	}
 
 	return (
 		<div className="overflow-hidden w-full h-screen absolute pointer-events-none">
 			<section className={`absolute top-0 ${show ? "right-0" : "-right-full"} transition-all w-full dark:bg-verydarkblue pointer-events-auto`}>
-				<button className="p-3 dark:bg-darkblue rounded-[4px] hover:opacity-80 active:scale-95" onClick={() => setShow(false)}>Back</button>
+				<button className="p-3 dark:bg-darkblue rounded-[4px] hover:opacity-80 active:scale-95" onClick={handleBack}>Back</button>
 				<article className="flex justify-center gap-6 p-8 items-center flex-col sm:flex-row">
 					<img src={country.flags.svg} alt={country.name.common + " flag"} className="sm:w-1/2" />
 					<div className="sm:w-1/2 p-5">
