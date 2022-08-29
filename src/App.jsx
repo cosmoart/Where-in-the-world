55358e1dba7516756fde744107359ad696772845
CosmoArt <cosmohydra17@gmail.com>
2022-08-29 14:55:22 -0500

    styles update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Error404 from "./components/Error404"
 
 function App() {
 	const MAX_PAGES = 24;
+	const [allCountries, setAllCountries] = useState([]);
 
 	const [maxPages, setMaxPages] = useState(MAX_PAGES);
 
@@ -21,6 +22,7 @@ function App() {
 		fetch(url)
 			.then(res => res.json())
 			.then(data => {
+				setAllCountries(data);
 				setCountries(data)
 				if (location.pathname !== "/") {
 					let countryPath = data.find(el => el.name.common.toLowerCase().replace(/ /ig, "-") === location.pathname.slice(1).toLowerCase().replace(/ /ig, "-"));
@@ -39,8 +41,12 @@ function App() {
 			if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !busy) {
 				setMaxPages(max => max + MAX_PAGES);
 				busy = true;
+				console.log(window.scrollY, document.querySelector(".countryCard").offsetTop);
 				setTimeout(() => busy = false, 600);
 			}
+			document.querySelectorAll(".countryCard").forEach(item => {
+				if (window.scrollY >= item.offsetTop) item.style.outline = "4px solid red"
+			})
 		})
 
 		// Show the :active outline only in key down
@@ -54,8 +60,8 @@ function App() {
 		<>
 			<MainHeader />
 			{country && <CountryDetails country={country} setCountry={setCountry} />}
-			<CountriesForm getCountries={getCountries} />
-			<main className="flagsList grid gap-11 grid-cols-fill p-7 relative">
+			<CountriesForm setCountries={setCountries} allCountries={allCountries} />
+			<main className="flagsList grid gap-11 grid-cols-fill px-10 relative">
 				{loading
 					? <Loader />
 					: <Cards countries={countries} MAX_PAGES={maxPages} setCountry={setCountry} />
--- src/components/CountriesForm.jsx
+++ src/components/CountriesForm.jsx
@@ -1,31 +1,38 @@
 import { useRef } from "react";
 
-export default function CountriesForm({ getCountries }) {
+export default function CountriesForm({ setCountries, allCountries }) {
 	const SearchInput = useRef();
+	let SearchQuery = null;
+	let RegionQuery = null;
 
 	function handleRegion(e) {
-		getCountries(`https://restcountries.com/v3.1/region/${e.target.value}`);
+		RegionQuery = e.target.value
+		let countriesResult = allCountries.filter(country => country.region.toLowerCase().includes(e.target.value));
+		setCountries(countriesResult);
 	}
 
 	function handleSearch(e) {
-		let countryPath = data.find(el => el.name.common.toLowerCase().replace(/ /ig, "-") === location.pathname.slice(1).toLowerCase().replace(/ /ig, "-"));
-		
-		// if (SearchInput.current.value.trim()) {
-		// 	getCountries(`https://restcountries.com/v3.1/name/${SearchInput.current.value.trim()}`);
-		// } else {
-		// 	getCountries(`https://restcountries.com/v3.1/all`);
-		// }
+		SearchQuery = SearchInput.current.value.trim().toLowerCase()
+
+		console.log(RegionQuery);
+		console.log(RegionQuery ? "Evalua" : "True");
+		if (SearchInput.current.value.trim()) {
+			let countriesResult = allCountries.filter(country => country.name.common.toLowerCase().includes(SearchInput.current.value.trim().toLowerCase()) && (RegionQuery ? country.region.toLowerCase().includes(RegionQuery) : true))
+			setCountries(countriesResult);
+		} else {
+			setCountries(allCountries);
+		}
 	}
 
 	return (
-		<form className="flex justify-between p-4 sm:flex-row flex-col gap-2">
+		<form className="flex justify-between p-10 sm:flex-row flex-col gap-2">
 			<div className="relative sm:w-[45%]">
-				<input ref={SearchInput} type="search" name="country" placeholder='Search for a country...' onChange={handleSearch} className="p-3 shadow-3xl pl-10 dark:bg-darkblue rounded-[4px] w-full" />
-				<button type="button" aria-label='Search' onClick={handleSearch} className="absolute left-3 top-2/4 -translate-y-1/2 hover:opacity-80 active:scale-95">
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.5"><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0z" /></svg>
+				<input ref={SearchInput} type="search" name="country" placeholder='Search for a country...' onChange={handleSearch} className="p-3 shadow-3xl pl-14 dark:bg-darkblue rounded-[4px] w-full max-w-lg" disabled={allCountries[0] ? false : true} />
+				<button type="button" aria-label='Search' onClick={handleSearch} className="absolute left-5 top-2/4 -translate-y-1/2 btnHover" disabled={allCountries[0] ? false : true}>
+					<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" width="24" height="24" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2.5"><path strokeLinecap="round" strokeLinejoin="round" d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0z" /></svg>
 				</button>
 			</div>
-			<select name="region" onChange={handleRegion} className="dark:bg-darkblue p-2 rounded-[4px]">
+			<select name="region" onChange={handleRegion} className="dark:bg-darkblue p-2 rounded-[4px]" disabled={allCountries[0] ? false : true}>
 				<option value="" defaultValue hidden>Filter by Region</option>
 				<option value="africa">Africa</option>
 				<option value="america">America</option>
