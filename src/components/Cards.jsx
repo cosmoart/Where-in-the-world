7420da6403328899553a611ec1ed3a8fdad36483
CosmoArt <cosmohydra17@gmail.com>
2022-08-30 10:51:24 -0500

    Error icon

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -16,9 +16,8 @@ export default function Cards({ countries, MAX_PAGES, setCountry }) {
 		setCountry(card);
 	}
 
-	if (countries !== "Error") {
+	if (countries !== "Error" && !countries.message) {
 		return countries.map((item, index) => {
-
 			if (index < MAX_PAGES) {
 				return (
 					<figure key={index + 105} tabIndex="0" className="countryCard bg-white dark:bg-darkblue hover:!scale-105 transition-all rounded-md overflow-hidden cursor-pointer shadow-3xl opacity-40 translate-y-6 scale-75" id={"countryCard" + index} onClick={() => handleCard(item)}>
@@ -40,7 +39,8 @@ export default function Cards({ countries, MAX_PAGES, setCountry }) {
 	} else {
 		return (
 			<article className="absolute-center">
-				<h2 className="text-center text-xl font-extrabold">Opps... Something is wrong</h2>
+				<svg className="w-[10rem] h-[5rem] m-4" fill="#ff4848" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M23,7.44365081 L23,16.5563492 L16.5563492,23 L7.44365081,23 L1,16.5563492 L1,7.44365081 L7.44365081,1 L16.5563492,1 L23,7.44365081 Z M15.7279221,3 L8.27207794,3 L3,8.27207794 L3,15.7279221 L8.27207794,21 L15.7279221,21 L21,15.7279221 L21,8.27207794 L15.7279221,3 Z M12.0003283,17.9983464 C11.4478622,17.9983464 11,17.5506311 11,16.9983464 C11,16.4460616 11.4478622,15.9983464 12.0003283,15.9983464 C12.5527943,15.9983464 13.0006565,16.4460616 13.0006565,16.9983464 C13.0006565,17.5506311 12.5527943,17.9983464 12.0003283,17.9983464 Z M11.0029544,5.99834639 L13.0036109,5.99834639 L13.0036109,13.9983464 L11.0029544,13.9983464 L11.0029544,5.99834639 Z" fillRule="evenodd" /></svg>
+				<h2 className="text-center text-xl font-extrabold">{countries.message || "Opps... Something is wrong"}</h2>
 			</article>
 		)
 	}
